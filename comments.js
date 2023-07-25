1fa44a7cc8679a1223ec8bc1af52297f92fc49e2
Muyi663 <140513305+Muyi663@users.noreply.github.com>
2023-07-25 15:56:45 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,12 @@
+// Create web server application with Node.js and Express.js that will serve the following routes:
+// GET /comments - returns all comments from the database
+// GET /comments/new - returns a form for creating a new comment
+// POST /comments - creates a new comment
+// GET /comments/:id - returns the comment with the provided id
+// GET /comments/:id/edit - returns a form for editing the comment with the provided id
+// PUT /comments/:id - updates the comment with the provided id
+// DELETE /comments/:id - deletes the comment with the provided id
+// The comment model should have the following properties:
+// id - generated automatically
+// author - required, non-empty string
+// content - required, non-empty string
