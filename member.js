4ac831c366bef8d1873bbb74067a024fd3930044
Muyi663 <140513305+Muyi663@users.noreply.github.com>
2023-07-25 15:38:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberContent = document.getElementById("memberContent");
+    var skillsContent = document.getElementById("skillsContent");
+    var projectsContent = document.getElementById("projectsContent");
+    var contactContent = document.getElementById("contactContent");
+    var aboutContent = document.getElementById("aboutCo ntent");
